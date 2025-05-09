54c19e87fe72155c32df0ba776ad0d39ad523eb7
Cesar Huacho <cehuacho24@gmail.com>
2025-05-09 13:54:25 -0500

    Configuracion de environments

--- src/environments/environments.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  rapidApi: {
-    key: process.env['RAPID_API_KEY'] || '',
-    host: process.env['RAPID_API_HOST'] || ''
-  }
-};
---
+++ src/environments/environments.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false, // cambiar a true para prod
+  rapidApi: {
+    key: 'your_api_key_here',
+    host: 'your_api_host_here'
+  }
+};
